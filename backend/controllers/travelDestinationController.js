9555f56bf4622f03abf5a63d014e8d164f54d46f
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-01 20:04:35 -0300

    Started adding travelDestination Controller using Axios // unknown api

--- backend/controllers/travelDestinationController.js
+++ backend/controllers/travelDestinationController.js
@@ -1,10 +1,27 @@
 // handles requests related to travel destinations
 //  fetch destinations, add new ones, and manage destination
 
+const axios = require('axios');
 const Destination = require('../models/travelDestination');
 
+const API_URL = '' // Will fill out the API once i find a suitable one
+
 const DestinationController = {
-    //
+// Creating a new destination
+    createDestination: async (req, res) => {
+        try {
+            const { name, description, photo, country, continent, category } = req.body;
+            const newDestination = {
+                name,
+                description,
+                photo,
+                country,
+                continent,
+                category,
+            };
+            const response = await axios.post(API_URL)
+        }
+    }
 };
 
 module.exports = DestinationController;
