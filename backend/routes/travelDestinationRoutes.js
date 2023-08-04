7b9ab79647e8700fc6dda3f2bd03cd48002133c9
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-04 12:03:16 -0300

    moved travelRoute from index.js to travelDestinationRoute.js

--- backend/routes/travelDestinationRoutes.js
+++ backend/routes/travelDestinationRoutes.js
@@ -1,2 +1,15 @@
 // define API routes for travel destinations
 // link to controller js file to handle destination 
+
+const express = require('express');
+const router = express.Router();
+const DestinationController = require('../controllers/travelDestinationController');
+
+// Define routes for each destination
+router.get('./destinations', DestinationController.getALLDestinations);
+router.get('/destinations/:id', DestinationController.getDestinationById);
+router.post('/destinations', DestinationController.createDestination);
+router.put('/destinations/:id', DestinationController.updateDestination);
+router.delete('/destinations/:id', DestinationController.deleteDestination);
+
+module.exports = router;
