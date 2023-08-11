5b635afde3c5fed0b8dd0faa7893064f9b9cea2c
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-11 20:46:22 -0300

    updated home routes

--- backend/controllers/home-routes.js
+++ backend/controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-  const filePath = path.join(__dirname, '../frontend/public/landing.html');
+  const filePath = path.join(__dirname, '/frontend/public/landing.html');
   res.sendFile(filePath);
 });
 
