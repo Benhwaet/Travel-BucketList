16bc2913708067fb90986da88ac5aaadc05bfcd5
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-13 15:35:50 -0300

    really nothing changed

--- public/src/app.js
+++ public/src/app.js
@@ -33,28 +33,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     registrationForm.addEventListener('submit', async (event) => {
         event.preventDefault();
-    
+      
         const email = document.getElementById('registerEmail').value;
         const username = document.getElementById('registerUsername').value;
         const password = document.getElementById('registerPassword').value;
-    
+      
         const requestData = {
             email: email,
             username: username,
             password: password
         };
-    
+      
         try {
             const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/signup', {
                 method: 'POST',
                 body: JSON.stringify(requestData),
                 headers: { 'Content-Type': 'application/json' }
             });
-
+      
             if (response.ok) {
                 const responseData = await response.json();
                 console.log('Registration successful:', responseData);
-
+      
                 modal.style.display = 'none';
             } else {
                 const errorData = await response.json();
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } catch (error) {
             console.error('Registration error:', error);
         }
-    });
+      });
 
     loginForm.addEventListener('submit', async (event) => {
         event.preventDefault();
