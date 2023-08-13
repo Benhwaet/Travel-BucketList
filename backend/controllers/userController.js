95fa0ccd981f761f2fd17475ae236a75d5f468c2
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-13 16:21:59 -0300

    updated again

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,7 @@ const userController = {
         return res.status(400).json({ message: 'User not found' });
       }
 
-      const validPassword = await bcrypt.compare(req.body.password, userData.password);
+      const validPassword = await bcrypt.compare(req.body.password, user.password);
 
       if (!validPassword) {
         return res.status(400).json({ message: 'Invalid credentials' });
