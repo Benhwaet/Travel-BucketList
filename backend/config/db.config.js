9594e23443d7d096754319516b9554dc0e5b0532
Coral <cnight18@hotmail.com>
2023-08-13 09:47:31 -0300

    attempting for the 5000th time

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -16,3 +16,15 @@ module.exports = {
         dialect: 'mysql'
     }
 };
+
+
+// cloudinary config
+const cloudinary = require('cloudinary').v2;
+          
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+module.exports = cloudinary;
--- backend/server.js
+++ backend/server.js
@@ -39,3 +39,37 @@ app.listen(port, () => {
 
 const imageUpload = require('./routes/imageUploads');
 app.use('/api', imageUpload);
+
+
+// sorry i have to put cloudinary here 
+// if it needs to be move , please try it its not working for me 
+const multer = require('multer');
+const cloudinary = require('/backend/config/db.config'); // cloudinary config
+app.use(express.json());
+
+// multer for handling file uploads
+const upload = multer({ dest: 'tmp/' });
+
+// route for image upload
+app.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file provided' });
+    }
+
+    // uploads image to Cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      folder: 'travel_bucket', // our folder on cloudinary
+    });
+
+    res.json({ url: result.secure_url });
+  } catch (error) {
+    console.error('Image upload error:', error);
+    res.status(500).json({ error: 'Image upload failed' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
