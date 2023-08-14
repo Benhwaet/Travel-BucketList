4c8d5862c162e5d2da963d700c10e82998520421
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-13 23:35:18 -0300

    i think I'm mindlessly just changing un needed code now

--- public/src/app.js
+++ public/src/app.js
@@ -6,94 +6,90 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeBtn = document.getElementById('closeBtn');
     const registrationForm = document.getElementById('registrationForm');
     const loginForm = document.getElementById('loginFormElement');
-
   
     registerBtn.addEventListener('click', () => {
-        modal.style.display = 'block';
+      modal.style.display = 'block';
     });
-
-   
+  
     loginBtn.addEventListener('click', () => {
-        Lmodal.style.display = 'block';
+      Lmodal.style.display = 'block';
     });
-
- 
+  
     closeBtn.addEventListener('click', () => {
+      modal.style.display = 'none';
+      Lmodal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
         modal.style.display = 'none';
         Lmodal.style.display = 'none';
+      }
     });
-
- 
-    window.addEventListener('click', (event) => {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-            Lmodal.style.display = 'none';
-        }
-    });
-
-
+  
     registrationForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-      
-        const email = document.getElementById('registerEmail').value;
-        const username = document.getElementById('registerUsername').value;
-        const password = document.getElementById('registerPassword').value;
-      
-        const requestData = {
-            email: email,
-            username: username,
-            password: password
-        };
-      
-        try {
-            const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/signup', {
-                method: 'POST',
-                body: JSON.stringify(requestData),
-                headers: { 'Content-Type': 'application/json' }
-            });
-      
-            if (response.ok) {
-                const responseData = await response.json();
-                console.log('Registration successful:', responseData);
-      
-                modal.style.display = 'none';
-            } else {
-                const errorData = await response.json();
-                console.error('Registration error:', errorData);
-            }
-        } catch (error) {
-            console.error('Registration error:', error);
-        }
-      });
-
-      const loginFormHandler = async (event) => {
-        event.preventDefault();
-        
-        const email = document.querySelector('#loginEmail').value.trim();
-        const password = document.querySelector('#loginPassword').value.trim();
-      
-        if (email && password) {
-          const requestData = { email, password };
-      
-          try {
-            const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/login', {
-              method: 'POST',
-              body: JSON.stringify(requestData),
-              headers: { 'Content-Type': 'application/json' },
-            });
-      
-            if (response.ok) {
-              document.location.replace('/');
-            } else {
-              const errorData = await response.json();
-              alert('Failed to log in: ' + errorData.message);
-            }
-          } catch (error) {
-            console.error('Login error:', error);
-          }
-        }
+      event.preventDefault();
+  
+      const email = document.getElementById('registerEmail').value;
+      const username = document.getElementById('registerUsername').value;
+      const password = document.getElementById('registerPassword').value;
+  
+      const requestData = {
+        email: email,
+        username: username,
+        password: password
       };
-      
-      loginForm.addEventListener('submit', loginFormHandler);
-      
+  
+      try {
+        const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/signup', {
+          method: 'POST',
+          body: JSON.stringify(requestData),
+          headers: { 'Content-Type': 'application/json' }
+        });
+  
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log('Registration successful:', responseData);
+  
+          modal.style.display = 'none';
+        } else {
+          const errorData = await response.json();
+          console.error('Registration error:', errorData);
+        }
+      } catch (error) {
+        console.error('Registration error:', error);
+      }
     });
+  
+    const loginFormHandler = async (event) => {
+      event.preventDefault();
+  
+      const email = document.querySelector('#loginEmail').value.trim();
+      const password = document.querySelector('#loginPassword').value.trim();
+  
+      const requestData = {
+        email: email,
+        password: password
+      };
+  
+      try {
+        const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/login', {
+          method: 'POST',
+          body: JSON.stringify(requestData),
+          headers: { 'Content-Type': 'application/json' }
+        });
+  
+        if (response.ok) {
+          document.location.replace('/');
+        } else {
+          const errorData = await response.json();
+          alert('Failed to log in: ' + errorData.message);
+        }
+      } catch (error) {
+        console.error('Login error:', error);
+      }
+    };
+  
+    loginForm.addEventListener('submit', loginFormHandler);
+  });
+  
