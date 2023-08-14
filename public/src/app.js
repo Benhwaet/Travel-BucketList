284bd36486cf81a6fe3f9117e0a746e84302d22f
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-13 23:14:57 -0300

    cant figure this out locally, added some console.logs to help me figure out whats going on

--- public/src/app.js
+++ public/src/app.js
@@ -68,24 +68,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const loginFormHandler = async (event) => {
         event.preventDefault();
-    
+        
         const email = document.querySelector('#loginEmail').value.trim();
         const password = document.querySelector('#loginPassword').value.trim();
-    
+      
         if (email && password) {
-          const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
-          });
-    
-          if (response.ok) {
-            document.location.replace('/');
-          } else {
-            alert('Failed to log in');
+          const requestData = { email, password };
+      
+          try {
+            const response = await fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/user/login', {
+              method: 'POST',
+              body: JSON.stringify(requestData),
+              headers: { 'Content-Type': 'application/json' },
+            });
+      
+            if (response.ok) {
+              document.location.replace('/');
+            } else {
+              const errorData = await response.json();
+              alert('Failed to log in: ' + errorData.message);
+            }
+          } catch (error) {
+            console.error('Login error:', error);
           }
         }
       };
-    
+      
       loginForm.addEventListener('submit', loginFormHandler);
+      
     });
