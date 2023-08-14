d0dc2c54b92c4c4c5dab342a2ed043a0c89a3a82
Coral <cnight18@hotmail.com>
2023-08-13 23:49:12 -0300

    more updates

--- public/src/sections/Journal.js
+++ public/src/sections/Journal.js
@@ -1,3 +1,62 @@
+// new code 
+document.addEventListener('DOMContentLoaded', function () {
+  const pages = document.querySelectorAll('.page'); // all page elements
+  const editorText = document.querySelector('.editor-text'); // text editor input
+  const addTextBtn = document.querySelector('.add-text-btn'); // add text button
+  const deleteButton = document.querySelector('.delete-btn'); // delete button 
+
+  let activePage = null; 
+
+  // cycle through each page add click event
+  pages.forEach((page, index) => {
+    page.pageNum = index + 1; // Assign a page number to each page ** it wont go past 3 pages why ? **
+
+    page.addEventListener('click', function () {
+      //  flip class and set activePage to the clicked page 
+      if (this.pageNum % 2 === 0) {
+        this.classList.remove('flipped');
+        this.previousElementSibling.classList.remove('flipped');
+      } else {
+        this.classList.add('flipped');
+        this.nextElementSibling.classList.add('flipped');
+      }
+      activePage = this; // Set the activePage to the clicked page ** trying to get this to work better ** 
+    });
+  });
+
+  // Add text to the active page when the add text button is clicked ** its laggy af **
+  addTextBtn.addEventListener('click', function () {
+    if (activePage) {
+      const text = editorText.value; 
+      const textElement = document.createElement('div'); 
+      textElement.classList.add('text'); 
+      textElement.innerText = text; 
+      activePage.appendChild(textElement); 
+
+      // saves text to  server 
+      fetch('https://traveling-bucket-a1886f9c05bf.herokuapp.com/api/journal/entries', {
+        method: 'POST',
+        body: JSON.stringify({ text: text }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => console.log('Text saved:', data))
+      .catch(error => console.error('Error saving text:', error));
+    }
+  });
+
+  //  delete button click to remove text
+  deleteButton.addEventListener('click', function () {
+    if (activePage) {
+      const textElements = activePage.querySelectorAll('.text'); //  all text  on the active page
+      textElements.forEach(element => element.remove()); // Remove each text element ** now idea how we should handle deleting **
+    }
+  });
+});
+
+
 
 //  *** BENS CODE BELOW I TRIED TO UPDATE *** *** may delete if this new code works better ***
 // // Select the journal section and delete entry buttons
