ffce5716ee2bb49a9f5ebba1975e0752b5d1990d
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-15 16:03:11 -0300

    changed

--- server.js
+++ server.js
@@ -37,53 +37,6 @@ app.use('/api/journal', journalRoutes)
 
 //// cloudinary
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDNAME,
-  api_key: process.env.CLOUDAPIKEY,
-  api_secret: process.env.CLOUDINARYSECRET,
-  secure: true
-});
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'profile.html'));
-});
-
-app.get('/get-signature', (req, res) => {
-  const timestamp = Math.round(new Date().getTime() / 1000);
-  const signature = cloudinary.utils.api_sign_request(
-    {
-      timestamp: timestamp
-    },
-    cloudinary.api_secret
-  );
-  res.json({ timestamp, signature });
-});
-
-app.post('/do-something-with-photo', async (req, res) => {
-  const expectedSignature = cloudinary.utils.api_sign_request(
-    { public_id: req.body.public_id, version: req.body.version },
-    cloudinary.api_secret
-  );
-
-  if (expectedSignature === req.body.signature) {
-    await fse.ensureFile('./data.txt');
-    const existingData = await fse.readFile('./data.txt', 'utf8');
-    await fse.outputFile('./data.txt', existingData + req.body.public_id + '\n');
-  }
-  res.status(200).send('Photo data processed.');
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Internal server error' });
-});
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
