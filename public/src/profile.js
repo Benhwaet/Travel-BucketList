1a3f0b6b971bdd4386b035f4aeba06b87c250a05
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-15 17:04:26 -0300

    test

--- public/src/profile.js
+++ public/src/profile.js
@@ -61,25 +61,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   
-var myWidget = cloudinary.createUploadWidget(
-  {
-    cloudName: "dstjbcoj0",
-    uploadPreset: "nkle7m7w"
-  },
-  (error, result) => {
-    if (!error && result && result.event === "success") {
-      console.log("done! here is the image info: ", result.info);
+  var myWidget = cloudinary.createUploadWidget({
+    cloudName: 'dstjbcoj0', 
+    uploadPreset: 'nkle7m7w'}, (error, result) => { 
+      if (!error && result && result.event === "success") { 
+        console.log('Done! Here is the image info: ', result.info); 
+      }
     }
-  }
-);
-
-document.getElementById("upload_widget").addEventListener(
-  "click", function() {
-    myWidget.open()
-  },
-  false
-);
-
+  )
+  
+  document.getElementById("upload_widget").addEventListener("click", function(){
+      myWidget.open();
+    }, false);
   // //memories input
   // import React from 'react';
   // import ReactDOM from 'react-dom';
