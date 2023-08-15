2da030483806a0bf55ee5154185780e0364d65e9
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-15 10:27:31 -0300

    added express.static for src to hopefully help solve our 404 errors

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.json());
 //app.use(express.static(__dirname + '/public'));;
 
 app.use(express.static('public'));
+app.use(express.static('src'));
 
 app.use('/node_modules', express.static('node_modules'));
 
