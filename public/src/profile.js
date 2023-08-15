845fb53c7c54de9b72a7072ac0cbf37353fa8005
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-08-15 16:39:54 -0300

    trying cloud widget

--- public/src/profile.js
+++ public/src/profile.js
@@ -61,7 +61,24 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
   
+var myWidget = cloudinary.createUploadWidget(
+  {
+    cloudName: "dstjbcoj0",
+    uploadPreset: "nkle7m7w"
+  },
+  (error, result) => {
+    if (!error && result && result.event === "success") {
+      console.log("done! here is the image info: ", result.info);
+    }
+  }
+);
 
+document.getElementById("upload_widget").addEventListener(
+  "click", function() {
+    myWidget.open()
+  },
+  false
+);
 
   // //memories input
   // import React from 'react';
